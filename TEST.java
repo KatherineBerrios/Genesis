b9a5fe9fffd30ab004e10a38f302178665385d5e
KatherineBerrios <44624787+KatherineBerrios@users.noreply.github.com>
2018-11-16 15:15:49 -0300

    Add files via upload

---
+++ TEST.java
@@ -0,0 +1,15 @@
+
+public class TEST {
+
+	public static void main(String[] args) {
+int a=3;
+int b=4;
+int suma;
+	suma=a+b;
+System.out.println("La suma es "+suma);
+int resta;
+	resta=a-b;
+System.out.println("La resta es "+resta);	
+	}
+
+}
