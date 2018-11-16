56f63ab54b302f7f2a8c1613bb4d19484b6fe7df
KatherineBerrios <44624787+KatherineBerrios@users.noreply.github.com>
2018-11-16 15:17:59 -0300

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,16 @@
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double c=3.2;
+int d=4;
+double multiplicación;
+	multiplicación=c*d;
+System.out.println ("El producto es "+multiplicación);
+double división;
+	división=c/d;
+System.out.println ("El cociente es "+división);
+	}
+
+}
