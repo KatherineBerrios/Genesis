f4606e665224f5451bc340c584314bde92076a4c
KatherineBerrios <44624787+KatherineBerrios@users.noreply.github.com>
2018-11-16 15:14:43 -0300

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	float c=5.6f;
+	int d=2;
+	float división;
+		división=c/d;
+	System.out.println ("El cociente es "+división);
+	float multiplicación;
+		multiplicación=c*d;
+	System.out.println ("El producto es "+multiplicación);
+	}
+
+}
